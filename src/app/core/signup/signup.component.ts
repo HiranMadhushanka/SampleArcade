4c6374c90eed3160320a33d2e2561b16ebe3be3d
Prasadi Sewwandi <akuramanager@gmail.com>
2021-08-02 23:46:16 +0530

    Signup form validations added

--- src/app/core/signup/signup.component.ts
+++ src/app/core/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,11 +10,35 @@ import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 })
 export class SignupComponent implements OnInit {
 
+  validationForm: FormGroup | any;
+
   constructor(public modalRef: MdbModalRef<SignupComponent>) { }
 
   ngOnInit(): void {
+    this.validationForm = new FormGroup({
+      userName: new FormControl(null, { validators: Validators.required, updateOn: 'blur' }),
+      Email: new FormControl(null, { validators: Validators.required, updateOn: 'blur' }),
+      country: new FormControl(null, { validators: Validators.required, updateOn: 'blur' }),
+      city: new FormControl(null, { validators: Validators.required, updateOn: 'blur' }),
+    });
   }
 
-  
+  get userName(): AbstractControl {
+    return this.validationForm.get('userName');
+  }
+
+  get Email(): AbstractControl {
+    return this.validationForm.get('Email');
+  }
+
+  get country(): AbstractControl {
+    return this.validationForm.get('country');
+  }
+
+  get city(): AbstractControl {
+    return this.validationForm.get('city');
+  }
+
+
 
 }
